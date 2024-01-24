fa83873792100b3aaf57283c4cb2e7cef42fdaef
Vinh Cuong <vinhcuongqbh@gmail.com>
2024-01-24 21:42:40 +0700

    24.01.2024

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return view('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
